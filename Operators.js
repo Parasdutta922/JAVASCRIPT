9e4f6318e4b0e00ec71aa75054d1a8a84259aace
Parasdutta922 <parasdutta922999@gmail.com>
2024-07-30 08:57:11 +0530

    First

---
+++ Operators.js
@@ -0,0 +1,21 @@
+//Arithmetic Operator
+
+let b = 0;
+b = b + 2; // '+' addition operator 
+console.log(b);
+
+b = b - 1; // '-' subtraction operator
+console.log(b);
+
+b = b * 4; // '*' Multiplication operator 
+console.log(b);
+
+b = b / 2; // '/' Division operator 
+console.log(b);
+
+
+//Assignment Operator
+
+let a = 0; // '=' Assigns the value
+
+
