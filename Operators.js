250b7a46b6957a7f1b10046ff606ead4c3d41b7c
Parasdutta922 <parasdutta922999@gmail.com>
2024-07-30 09:37:54 +0530

    Second

--- Operators.js
+++ Operators.js
@@ -7,15 +7,67 @@ console.log(b);
 b = b - 1; // '-' subtraction operator
 console.log(b);
 
-b = b * 4; // '*' Multiplication operator 
+b = b * 9; // '*' Multiplication operator 
 console.log(b);
 
 b = b / 2; // '/' Division operator 
 console.log(b);
 
+b = b % 2; // '%' Remainder operator
+console.log(b);
 
-//Assignment Operator
+b++; // '++' Increment Operator increases by 1 
+console.log(b);
+
+b--; // '--' Decrement Operator decreases by 1
+console.log(b);
+
+b = b**2
+console.log(b);
+
+//Assignment Operators
 
 let a = 0; // '=' Assigns the value
+
+a += 2; 
+console.log(a);
+
+a -= 1;
+console.log(a);
+
+a *= 8;
+console.log(a);
+
+a /= 2;
+console.log(a);
+
+a %= 3;
+console.log(a);
+
+a **= 2;
+console.log(a);
+
+// Comparison operator 
+
+const c = 2;
+const d = 5;
+const e = '5';
+
+console.log(c > d);
+console.log(c < d);
+console.log(c >= d);
+console.log(c <= d);
+console.log(c == d);
+console.log(d === e);
+console.log(c != d);
+console.log(d !== e);
+
+// Logical Operator
+
+console.log((c < d) && (d !== e));
+console.log((c > d) || (d > c));
+console.log(!c);
+
+(c>d) ? console.log('Greater') : console.log('Not Fret');
 
 
